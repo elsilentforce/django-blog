eb342a3405b196aa1f310d271c647b87f23552fa
Patricio Tapia <native.devil@gmail.com>
2021-10-08 17:42:57 -0300

    REFACT: Bad body datatype declaration

--- blog/models.py
+++ blog/models.py
@@ -11,7 +11,7 @@ class Post(models.Model):
   title = models.CharField(max_length=250)
   slug = models.CharField(max_length=250, unique_for_date='publish')
   author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
-  body = models.TextField
+  body = models.TextField()
   publish = models.DateTimeField(default=timezone.now)
   created = models.DateTimeField(auto_now_add=True)
   updated = models.DateTimeField(auto_now=True)
