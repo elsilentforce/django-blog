e761d76a9d6d1e96283e1300761678347e869682
Patricio Tapia <native.devil@gmail.com>
2021-10-09 04:45:39 -0300

    ADD: Cannonical url method

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.urls import reverse
 from django.contrib.auth.models import User
 
 # Create your models here.
@@ -25,6 +26,10 @@ class Post(models.Model):
   # Managers
   objects = models.Manager()
   published = PublishedManager()
+
+  def get_absolute_url(self):
+      return reverse("blog:post_detail", args=[self.publish.year, self.publish.month, self.publish.day, self.slug])
+  
 
   class Meta:
     ordering = ['-publish']
