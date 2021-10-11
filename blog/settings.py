45738bca055397fe6c08c7448284c5ed5796d27b
Patricio Tapia <native.devil@gmail.com>
2021-10-10 22:53:51 -0300

    ADD: Email sending configs

---
+++ blog/settings.py
@@ -0,0 +1,7 @@
+EMAIL_HOST = 'localhost'
+EMAIL_PORT = 25
+# EMAIL_HOST_USER =
+# EMAIL_HOST_PASSWORD =
+# EMAIL_USE_TLS =
+# EMAIL_USE_SSL =
+EMAIL_BACKEND = 'django.mail.backends.console.EmailBackend'
