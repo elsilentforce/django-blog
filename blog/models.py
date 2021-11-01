785978f85ceb063f7523c5241bfb22969078c041
Patricio Tapia <native.devil@gmail.com>
2021-11-01 10:33:04 -0300

    ADD: Post tags migration

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.urls import reverse
+from django.contrib.auth.models import User
 from taggit.managers import TaggableManager
 
 # Create your models here.
